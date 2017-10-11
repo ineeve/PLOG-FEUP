d13c38db98e58342b1eb34637ed15f3a60b5e7d8
ineeve <renatoineeve@gmail.com>
2017-10-11 18:57:04 +0100

    Display Board added

--- src/CampoBello.pl
+++ src/CampoBello.pl
@@ -56,8 +56,20 @@ pontosB([H|T],P) :- pontosB(T,P), (member(H,boardB) -> P is P + 3; P is P + 1).
 pontosY([],0).
 pontosY([H|T],P) :- pontosY(T,P), (member(H,boardY) -> P is P + 3; P is P + 1).
 
-
 pontos(Player,Pontos) :- (Player == b -> pecasB(X), pontosB(X,Pontos); pecasY(X),pontosY(X,Pontos)).
 
-
 isConnected(X,Y) :- boardMember(B), member(X,B), member(Y,B), (connected(X,L), member(Y,L)) ; (connected(Y,L), member(X,L)).
+
+displaySingleP(P) :-(pecasY(Y), member(P,Y) -> write(y); pecasB(B), member(P,B) -> write(b); write(e)), write(' ').
+displayPos([H|T]) :- displaySingleP(H), displayPos(T).
+displayPos([]).
+displayLine(1) :- write('   '),displayPos([g9,g8,g7,g6]),nl.
+displayLine(2) :- displaySingleP(b6), write('  '), displayPos([g5,g4,g3]), write('  '), displaySingleP(r9),nl.
+displayLine(3) :- write(' '), displaySingleP(b3), write('  '), displayPos([g2,g1]), write('  '), displaySingleP(r5),nl.
+displayLine(4) :- displayPos([b7,b1]), write('  '), displaySingleP(g0), write('  '),displayPos([r2,r8]), nl.
+displayLine(5) :- write(' '), displaySingleP(b4), write(' '), displayPos([b0,mid,r0]),write(' '),displaySingleP(r4),nl.
+displayLine(6) :- displayPos([b8,b2]), write('  '), displaySingleP(y0), write('  '),displayPos([r1,r7]), nl.
+displayLine(7) :- write(' '), displaySingleP(b5), write('  '), displayPos([g1,g2]), write('  '), displaySingleP(r6),nl.
+displayLine(8) :- displaySingleP(b9), write('  '), displayPos([y3,y4,y5]), write('  '), displaySingleP(r6),nl.
+displayLine(9) :- write('   '),displayPos([y6,y7,y8,y9]),nl.
+displayBoard :- displayLine(1), displayLine(2), displayLine(3), displayLine(4),displayLine(5),displayLine(6),displayLine(7),displayLine(8),displayLine(9).
